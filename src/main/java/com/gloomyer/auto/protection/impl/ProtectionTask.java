52095a43bb2da1ba2c40b600fd0a153ab573747e
gloomy <gloomyneter@gmail.com>
2019-11-07 14:50:41 +0800

    乐固

--- src/main/java/com/gloomyer/auto/protection/impl/ProtectionTask.java
+++ src/main/java/com/gloomyer/auto/protection/impl/ProtectionTask.java
@@ -3,21 +3,22 @@ package com.gloomyer.auto.protection.impl;
 import com.gloomyer.auto.domain.ApkInfo;
 import com.gloomyer.auto.utils.ApkUtils;
 import com.gloomyer.auto.utils.LG;
+import com.qiniu.util.StringMap;
 import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.exception.TencentCloudSDKException;
 import com.tencentcloudapi.common.profile.ClientProfile;
 import com.tencentcloudapi.common.profile.HttpProfile;
 import com.tencentcloudapi.ms.v20180408.MsClient;
-import com.tencentcloudapi.ms.v20180408.models.CreateResourceInstancesRequest;
-import com.tencentcloudapi.ms.v20180408.models.CreateResourceInstancesResponse;
-import com.tencentcloudapi.ms.v20180408.models.CreateShieldInstanceRequest;
-import com.tencentcloudapi.ms.v20180408.models.CreateShieldInstanceResponse;
+import com.tencentcloudapi.ms.v20180408.models.*;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import java.io.File;
 
 class ProtectionTask implements Runnable {
+    private static final long DEFAULT_SLEEP_TIME = 1000 * 60 * 3;
+    private static final long DECREMENT_TIME = 1000 * 60;
+    private static final long MIN_TIME = 1000 * 40;
     private final File file;
     final String url;
     private final String secretId;
@@ -32,50 +33,75 @@ class ProtectionTask implements Runnable {
 
     @Override
     public void run() {
-        createProtectionRule();
-        //upload2LG();
+        String itemId = upload2LG();
+        JSONObject retInfo = query(itemId);
+        downloadApk(retInfo);
     }
 
     /**
-     * 创建加固策略
+     * 对加固成功的结果，进行下载APK
+     *
+     * @param info info
      */
-    private void createProtectionRule() {
-        Credential cred = new Credential(secretId, secretKey);
+    private void downloadApk(JSONObject info) {
 
-        HttpProfile httpProfile = new HttpProfile();
-        httpProfile.setEndpoint("ms.tencentcloudapi.com");
+    }
 
-        JSONObject obj = new JSONObject();
-        try {
-            obj.put("Pid", "13624");
-            obj.put("TimeUnit", "d");
-            obj.put("TimeSpan", 1);
-            obj.put("ResourceNum", 1);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
-        ClientProfile clientProfile = new ClientProfile();
-        clientProfile.setHttpProfile(httpProfile);
-        MsClient client = new MsClient(cred, "ap-shanghai", clientProfile);
-        CreateResourceInstancesRequest req = CreateResourceInstancesRequest
-                .fromJsonString(obj.toString(), CreateResourceInstancesRequest.class);
-        try {
-            CreateResourceInstancesResponse resp = client.CreateResourceInstances(req);
-            System.out.println(CreateResourceInstancesRequest.toJsonString(resp));
-        } catch (TencentCloudSDKException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
+    /**
+     * 加固任务查询
+     *
+     * @param itemId id
+     */
+    private JSONObject query(String itemId) {
+        long sleepTime = DEFAULT_SLEEP_TIME;
+        for (; ; ) {
+            try {
+                Thread.sleep(sleepTime);
+                sleepTime -= DECREMENT_TIME;
+                if (sleepTime <= MIN_TIME) {
+                    sleepTime = MIN_TIME;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            MsClient client = createMsClient();
+
+            String params = "{\"ItemId\":\"" + itemId + "\"}";
+            DescribeShieldResultRequest req =
+                    DescribeShieldResultRequest.fromJsonString(params, DescribeShieldResultRequest.class);
+            try {
+                DescribeShieldResultResponse resp = client.DescribeShieldResult(req);
+                String json = CreateResourceInstancesRequest.toJsonString(resp);
+                JSONObject obj = new JSONObject(json);
+                LG.e("ID:{0}\n查询结果:{1}", itemId, json);
+                if (resp.getTaskStatus() == 3) {
+                    //异常，删除，重新执行
+                    delete(itemId);
+                    return query(upload2LG());
+                } else if (resp.getTaskStatus() == 1) {
+                    return obj;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage());
+            }
         }
     }
 
-    private void upload2LG() {
-        Credential cred = new Credential(secretId, secretKey);
-        HttpProfile httpProfile = new HttpProfile();
-        httpProfile.setEndpoint("ms.tencentcloudapi.com");
-        ClientProfile clientProfile = new ClientProfile();
-        clientProfile.setHttpProfile(httpProfile);
-        MsClient client = new MsClient(cred, "", clientProfile);
+    private void delete(String itemId) {
+        try {
+            MsClient client = createMsClient();
+            String params = "{\"ItemIds\":[\"" + itemId + "\"]}";
+            DeleteShieldInstancesRequest req = DeleteShieldInstancesRequest.fromJsonString(params, DeleteShieldInstancesRequest.class);
+            DeleteShieldInstancesResponse resp = client.DeleteShieldInstances(req);
+            LG.e("删除ID:{0},结果:{1}", itemId, DeleteShieldInstancesRequest.toJsonString(resp));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String upload2LG() {
+        MsClient client = createMsClient();
 
         JSONObject params = new JSONObject();
         JSONObject appInfo = new JSONObject();
@@ -107,10 +133,24 @@ class ProtectionTask implements Runnable {
                         CreateShieldInstanceRequest.class);
         try {
             CreateShieldInstanceResponse resp = client.CreateShieldInstance(req);
-            System.out.println(CreateShieldInstanceRequest.toJsonString(resp));
-        } catch (TencentCloudSDKException e) {
+            JSONObject obj = new JSONObject(CreateShieldInstanceRequest.toJsonString(resp));
+            LG.e("加固结果:{0}", obj.toString());
+            return obj.getString("ItemId");
+        } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    private MsClient createMsClient() {
+        Credential cred = new Credential(secretId, secretKey);
+
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setEndpoint("ms.tencentcloudapi.com");
+//
+        ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setHttpProfile(httpProfile);
+
+        return new MsClient(cred, "ap-shanghai", clientProfile);
     }
 }
